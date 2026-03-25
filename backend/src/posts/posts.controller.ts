01929d006fc415e3da25704ac9dcd60254725027
tkirmizi <t.kirmizioglu@outlook.com>
2026-03-25 10:50:41 +0100

    add basic post fundementals

---
+++ backend/src/posts/posts.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+import { CreatePostDto } from './create-post.dto';
+import { PostsService } from './posts.service';
+
+type JwtRequest = {
+  user: {
+    id: number;
+    email: string;
+  };
+};
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  async list() {
+    return this.postsService.list();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(@Request() req: JwtRequest, @Body() body: CreatePostDto) {
+    return this.postsService.create(req.user.id, body.content);
+  }
+}
