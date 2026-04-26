563ee6c17f32ef965f4b0c8af9389a0bdd8868b8
tkirmizi <t.kirmizioglu@outlook.com>
2026-04-26 12:28:57 +0200

    add https for backend, certs generated at build in docker with ssl /app/certs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as path from "path";
+import * as fs from "fs";
 import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const httpsKeyPath = process.env.HTTPS_KEY_PATH;
+  const httpsCertPath = process.env.HTTPS_CERT_PATH;
+  const httpsOptions =
+    httpsKeyPath && httpsCertPath
+      ? {
+          key: fs.readFileSync(httpsKeyPath),
+          cert: fs.readFileSync(httpsCertPath),
+        }
+      : undefined;
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    ...(httpsOptions ? { httpsOptions } : {}),
+  });
   app.enableCors({ origin: true, credentials: true });
   
   // Serve uploaded files as static assets
